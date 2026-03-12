d3cae0818acf121d77c2476271a0be9a1315cf86
sihoyang1-max <sihoyang1@gmail.com>
2026-03-12 14:56:18 +0900

    2

---
+++ My project (10)/Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public string playerName;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Heloo Unity!")
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
